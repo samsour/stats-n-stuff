466fe62aa8eb971b55b5b12fa43a7dd167dd20e8
Sam Sauer <s.sauer@syzygy.de>
2018-03-15 14:47:18 +0100

    refactored Clock.js

--- src/assets/js/Clock.js
+++ src/assets/js/Clock.js
@@ -11,46 +11,49 @@ export default class Clock {
 
 	constructor(clockClass) {
 		this._clockClass = clockClass;
+		this.checkDate();
 	}
 
 
 	startTimer() {
-
 		this._interval = setInterval(() => {
-
-			let today = new Date();
-			let h = today.getHours();
-			let m = today.getMinutes();
-			let s = today.getSeconds();
-			
-			let weekday = new Array(7);
-			weekday[0] =  "So";
-			weekday[1] = "Mo";
-			weekday[2] = "Die";
-			weekday[3] = "Mi";
-			weekday[4] = "Do";
-			weekday[5] = "Fr";
-			weekday[6] = "Sa";
-
-			let date = today.getDate();
-			let month = today.getMonth();
-
-			$(this._clockClass).html(
-				'<span class="smaller">'
-				+ weekday[today.getDay()]
-				+ '</span>'
-				+ '<span class="small">'
-				+ this.checkNumber(date) + '.' + this.checkNumber(parseInt(month+1))
-				+ '</span>'
-				+ '<span class="larger">'
-				+ h + ':' + this.checkNumber(m)
-				+ '</span>'
-				+ '<span class="small">'
-				+ this.checkNumber(s)
-				+ '</span>'
-		)}, 1000);
+		this.checkDate();
+		}, 1000);
 	}
 
+	checkDate() {
+		let today = new Date();
+		let h = today.getHours();
+		let m = today.getMinutes();
+		let s = today.getSeconds();
+		
+		let weekday = new Array(7);
+		weekday[0] =  "So";
+		weekday[1] = "Mo";
+		weekday[2] = "Die";
+		weekday[3] = "Mi";
+		weekday[4] = "Do";
+		weekday[5] = "Fr";
+		weekday[6] = "Sa";
+
+		let date = today.getDate();
+		let month = today.getMonth();
+
+		$(this._clockClass).html(
+			'<span class="smaller">'
+			+ weekday[today.getDay()]
+			+ '</span>'
+			+ '<span class="small">'
+			+ this.checkNumber(date) + '.' + this.checkNumber(parseInt(month+1))
+			+ '</span>'
+			+ '<span class="larger">'
+			+ h + ':' + this.checkNumber(m)
+			+ '</span>'
+			+ '<span class="small">'
+			+ this.checkNumber(s)
+			+ '</span>'
+		);
+	}
 
 	// add zero in front of numbers < 10
 	checkNumber(i) {
@@ -58,9 +61,11 @@ export default class Clock {
 		return i;
 	}
 
+	// display analog clock
+	// in progress
+	//
     // let hourRotate = "rotate(" + ((seconds % 43200) * 360 / 43200 + 180) + "deg)";
     // let minuteRotate = "rotate(" + ((seconds % 3600) * 360 / 3600 + 180) + "deg)";
     // let secondRotate = "rotate(" + ((seconds % 60) * 360 / 60 + 180) + "deg)";
-
 }
 
