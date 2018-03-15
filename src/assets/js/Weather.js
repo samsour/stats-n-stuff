ba55ae9587684f63be500f70955219bfdcc587b9
Sam Sauer <s.sauer@syzygy.de>
2018-03-15 14:47:31 +0100

    added Weather.js

---
+++ src/assets/js/Weather.js
@@ -0,0 +1,47 @@
+import $ from 'jquery';
+
+export default class Weather {
+    constructor() {
+        this.getWeather();
+    }
+
+    getWeather() {
+        // weather api call
+        $.ajax({
+            url: 'https://api.apixu.com/v1/current.json?key=2c8bf56be4704deeb4b102456181403&q=Frankfurt&lang=de',
+            dataType: 'json',
+            success: function(data) {
+                console.log("Weather JSON: ", data);
+    
+                $('.weather-icon').html('<img src="' + data.current.condition.icon + '">');
+                // $('.weather-status').html(data.current.condition.code);
+
+                let temperature = data.current.temp_c;
+                let feelsLike = data.current.feelslike_c;
+                let location = data.location.name;
+                let conditionText = data.current.condition.text;
+                let conditionCode = data.current.condition.code;
+
+                $('.weather-temperature').html(
+                    'In '
+                    + location
+                    + ' sind es ' 
+                    + temperature 
+                    + '°C und '
+                    + conditionText 
+                    + '. Es fühlt sich ' 
+                    + checkPhrase(temperature, feelsLike) 
+                    + ' an wie ' + data.current.feelslike_c + '°C.'
+                );
+                
+                function checkPhrase(temp1, temp2) {
+                    if(temp1 == temp2) {
+                        return 'auch';
+                    } else {
+                        return 'aber';
+                    }
+                }
+            }
+        });
+    }
+}
--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,6 +1,7 @@
 import $ from 'jquery';
 import babelpolyfill from 'babel-polyfill';
 import Clock from './Clock';
+import Weather from './Weather';
 import User from './User';
 
 let currentWeatherObj = null;
@@ -9,48 +10,43 @@ $(document).ready(function() {
 
     let clock = new Clock('.clock');
     clock.startTimer();
+    
 
-    // weather api call
-    $.ajax({
-        url: 'https://api.apixu.com/v1/current.json?key=2c8bf56be4704deeb4b102456181403&q=Frankfurt&lang=de',
-        dataType: 'json',
-        success: function(data) {
-            console.log(data);
-
-            $('.weather-icon').html('<img src="' + data.current.condition.icon + '">');
-            // $('.weather-status').html(data.current.condition.text);
-            let currentTemp = data.current.temp_c;
-            let currentFeelslike = data.current.feelslike_c;
-            $('.weather-temperature').html(
-                'In '
-                + data.location.name
-                + ' ist es ' 
-                + data.current.condition.text 
-                + ' mit ' + data.current.temp_c 
-                + '°C. Es fühlt sich ' 
-                + checkPhrase(currentTemp, currentFeelslike) 
-                + ' an wie ' + data.current.feelslike_c + '°C'
-            );
-            
-            function checkPhrase(temp1, temp2) {
-                if(temp1 == temp2) {
-                    return 'auch';
-                } else {
-                    return 'aber';
-                }
-            }
-        }
-    });
-
-    // rmv api call
-    $.ajax({
-        url: 'https://www.rmv.de/hapi/location.name?accessId=a0d5a70b-d413-4863-a0b9-67ddfef252d6&input=frankfurt%20hauptbahnhof',
-        dataType: 'json',
-        success: function(data) {
-            console.log(data);
-        }
-    })
+    let weather = new Weather();
 })
+
+// function setBackground() {
+
+//     $('body').removeClass();
+
+//     switch(data) {
+//         case data == 100:
+//             $('body').addClass('sunny');
+//             break;
+//         case data == 101:
+//             $('body').addClass('rainy');
+//             break;
+//         case data == 102:
+//             $('body').addClass('mostly-clouds');
+//             break;
+//         default:
+//             $('body').addClass('neutral');
+//             break;
+//     }
+// }
+
+
+
+// function rmvApi() {
+//     // rmv api call
+//     $.ajax({
+//         url: 'https://www.rmv.de/hapi/location.name?accessId=a0d5a70b-d413-4863-a0b9-67ddfef252d6&input=frankfurt%20hauptbahnhof',
+//         dataType: 'json',
+//         success: function(data) {
+//             console.log(data);
+//         }
+//     })
+// }
 
 /*
 
