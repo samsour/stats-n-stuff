e58508644a15f40730be1924487defc8d2710ac5
samsour <sam.sauer@hotmail.de>
2018-03-16 14:16:51 +0100

    refactored Coin.js

--- src/assets/js/Coin.js
+++ src/assets/js/Coin.js
@@ -6,32 +6,32 @@ export default class Coin {
     set name(name) { this._name = name };
     
     get price() { return this._price };
-    set price(price) { this._price = price };
 
-    get percent_change_1h() { return this._percent_change_1h};
-    get percent_change_1h() { return this._percent_change_1h};
+    get percentChange1h() { return this._percentChange1h };
 
-    get percent_change_24h() { return this._percent_change_24h};
-    get percent_change_24h() { return this._percent_change_24h};
+    get percentChange24h() { return this._percentChange24h};
 
-    get percent_change_7d() { return this._percent_change_7d};
-    get percent_change_7d() { return this._percent_change_7d};
+    get percentChange7d() { return this._percentChange7d};
 
-    get lastUpdated() { return this._lastUpdated};
     get lastUpdated() { return this._lastUpdated};
     
 	constructor(url) {
-		this.getData(url);
+        this.getData(url);
     }
     
-    getData(coinUrl) {
+    getData(url) {
+
+        let self = this;
+
         $.ajax({
             //url: 'https://api.coinmarketcap.com/v1/ticker/?convert=EUR&limit=20',
-            url: coinUrl,
+            url: url,
             dataType: 'json',
             success: function(data) {
-                console.log("Coins JSON: ", data);
+            
+                console.log(data[0].name + " JSON: " , data);
 
+                // set class variables to api values
                 this._name = data[0].name;
                 this._price = data[0].price_eur;
                 this._percentChange1h = data[0].percent_change_1h;
@@ -39,100 +39,49 @@ export default class Coin {
                 this._percentChange7d = data[0].percent_change_7d;
                 this._lastUpdated = new Date(data[0].last_updated * 1000);
 
-            
-                console.log("Display coin: " + this._name);
+                self.createHtml(this._name);
 
-                $('.coin-name').html(this._name);
-                $('.coin-price').html(this._price);
-                $('.percent-change-1h').html(this._percentChange1h);
-                $('.percent-change-24h').html(this._percentChange24h);
-                $('.percent-change-7d').html(this._percentChange7d);
-                $('.last-updated').html(this._lastUpdated.getHours() + ":" + this._lastUpdated.getMinutes());
+                // print in html
+                let className = '.' + this._name.toLowerCase();
                 
+                $(className + ' .coin-name').html(this._name);
 
-                // $('.coins').html(
-                //     'In '
-                //     + location
-                //     + ' sind es ' 
-                //     + temperature 
-                //     + '°C und '
-                //     + conditionText 
-                //     + '. Es fühlt sich ' 
-                //     + checkPhrase(temperature, feelsLike) 
-                //     + ' an wie ' + data.current.feelslike_c + '°C.'
-                // );
+                $(className + ' .coin-price').html(parseFloat(this._price).toFixed(4) + '€');
+
+                let change1h = self.checkPercentage(this._percentChange1h);
+                $(className + ' .percent-change-1h').html(change1h + ' (1H)');
+
+                let change24h = self.checkPercentage(this._percentChange24h);
+                $(className + ' .percent-change-24h').html(change24h + ' (24H)');
+
+                let change7d = self.checkPercentage(this._percentChange7d);
+                $(className + ' .percent-change-7d').html(change7d + ' (7D)');
                 
-                // function checkPhrase(temp1, temp2) {
-                //     if(temp1 == temp2) {
-                //         return 'auch';
-                //     } else {
-                //         return 'aber';
-                //     }
-                // }
+                let minutes = self.checkNumber(this._lastUpdated.getMinutes());
+                $(className + ' .last-updated').html(this._lastUpdated.getHours() + ":" + minutes);
             }
         });
     }
 
-    printResults() {
+    createHtml(coin) {
 
+        $('.coins').append('<div class="coin-item ' 
+        + coin.toLowerCase()
+        + '"><div class="left"><div class="coin-name"></div><div class="coin-price"></div><div class="last-updated"></div></div><div class="right"><div class="percent-change-1h"></div><div class="percent-change-24h"></div><div class="percent-change-7d"></div></div></div>')
+    }
+
+    checkNumber(i) {
+		if (i < 10) {i = "0" + i};  
+		return i;
+    }
+    
+    checkPercentage(i) {
+        if (i > 0) {
+            return '<span class="green">' + i + '%' + '</span>'
+        } else if (i < 0) {
+            return '<span class="red">' + i + '%' + '</span>'
+        } else {
+            return i + '%';
+        }
     }
 }
-
-/*
-
-24h_volume_eur
-:
-"30858557.6546"
-24h_volume_usd
-:
-"37988600.0"
-available_supply
-:
-"2779530283.0"
-id
-:
-"iota"
-last_updated
-:
-"1521188350"
-market_cap_eur
-:
-"2506702482.0"
-market_cap_usd
-:
-"3085890111.0"
-max_supply
-:
-"2779530283.0"
-name
-:
-"IOTA"
-percent_change_1h
-:
-"0.03"
-percent_change_7d
-:
-"-12.72"
-percent_change_24h
-:
-"1.53"
-price_btc
-:
-"0.00013458"
-price_eur
-:
-"0.9018439184"
-price_usd
-:
-"1.11022"
-rank
-:
-"13"
-symbol
-:
-"MIOTA"
-total_supply
-:
-"2779530283.0"
-
-*/
