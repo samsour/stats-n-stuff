c35384e45e4eb7c507615ddb07d680f8e33713b8
samsour <sam.sauer@hotmail.de>
2018-03-19 09:16:21 +0100

    changed train api datatype

--- src/assets/js/Train.js
+++ src/assets/js/Train.js
@@ -8,7 +8,7 @@ export default class Train {
     getData() {
         $.ajax({
             url: 'https://www.rmv.de/hapi/trip?accessId=a0d5a70b-d413-4863-a0b9-67ddfef252d6&format=json&originExtId=003000510&destExtId=003002498&numF=6&viaId=3060774&time=16:00',
-            dataType: 'xml',
+            dataType: 'json',
             success: function(data) {
                 console.log("RMV: ", data);
                 $('.trains').html(data);
