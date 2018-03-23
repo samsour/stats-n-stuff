323ca4d83099e78c1cb3b3b71b29ebc11be52836
samsour <sam.sauer@hotmail.de>
2018-03-23 23:56:36 +0100

    changeLocation function

--- src/assets/js/User.js
+++ src/assets/js/User.js
@@ -2,7 +2,12 @@ import $ from 'jquery';
 
 export default class User {
     
-    constructor(username) {
+    constructor(username, location) {
         this._name = username;
+        this._location = location;
+    }
+
+    changeLocation(location) {
+        this._location = location;
     }
 }
