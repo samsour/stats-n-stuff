5d9947e0253a4deaa9d3b7923f34a1b856e31190
samsour <sam.sauer@hotmail.de>
2020-01-15 15:45:50 +0100

    Add weather component and api handling

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <weather :location="'Frankfurt'" :language="'de'" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Weather from './components/Weather';
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Weather
   }
 }
 </script>
