32821c7ce1d7f07105a7eaa53fa1e1e1caa2b467
samsour <sam.sauer@hotmail.de>
2020-02-09 17:17:29 +0100

    Integrate stationBoardData

--- src/components/PublicTransport.vue
+++ src/components/PublicTransport.vue
@@ -1,26 +1,43 @@
 <template>
   <div class="public-transport">
+    <ul v-if="stationBoardData">
+      <li v-for="entry in stationBoardData" :key="entry.id">
+        <station-board-entry v-bind="entry" />
+      </li>
+    </ul>
     <ul>
-      <li v-for="stop in currentStops" :key="stop.id">{{ stop.name }}</li>
+      <li v-for="stop in currentStops" :key="stop.id">
+        <button @click="getStationBoard(stop.extId)">{{ stop.name }}</button>
+        </li>
     </ul>
   </div>
 </template>
 
 <script>
 import { mapGetters } from "vuex";
+import StationBoardEntry from "../components/StationBoardEntry";
 
 export default {
   name: "public-transport",
+  components: {
+    StationBoardEntry
+  },
   computed: {
     ...mapGetters({
         locationInput: "location",
-        currentStops: "RMV/currentStops"
+        currentStops: "RMV/currentStops",
+        stationBoardData: "RMV/currentStationBoardData"
     })
   },
   mounted() {
       if (this.locationInput) {
           this.$store.dispatch("RMV/fetchData");
       }
+  },
+  methods: {
+    getStationBoard(stationId) {
+      this.$store.dispatch("RMV/fetchStationBoardData", { stationId, type: 'arrival' });
+    }
   }
 };
 </script>
