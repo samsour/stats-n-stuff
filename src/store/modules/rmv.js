4687bc9ada7ef07cc0d0274726c7785dadb45a68
samsour <sam.sauer@hotmail.de>
2020-02-09 17:15:57 +0100

    Add api proxy to bypass cors issues

--- src/store/modules/rmv.js
+++ src/store/modules/rmv.js
@@ -1,22 +1,30 @@
-import api from "@/utils/rmv-api";
+import api from '@/utils/rmv-api';
+import { Capitalize } from '@/utils/helpers';
 
 export default {
     namespaced: true,
     state: {
         locationData: [],
+        currentStationBoardData: []
     },
     getters: {
-        currentStops: state => state.locationData
+        // get actual stops, not the meta objects
+        currentStops: state => state.locationData.length > 0 ? state.locationData.filter(stop => stop.meta !== true) : [],
+        currentStationBoardData: state => state.currentStationBoardData
     },
     mutations: {
         SET_LOCATION_DATA(state, data) {
             state.locationData = data.map(x => x.StopLocation)
         },
+
+        SET_CURRENT_STATION_BOARD(state, data) {
+            console.log(data);
+            state.currentStationBoardData = data;
+        },
     },
     actions: {
         fetchData({ commit, rootState }) {
-            const proxyurl = "https://cors-anywhere.herokuapp.com/";
-            fetch(`${proxyurl + api.baseUrl}location.name?accessId=${api.key}&input=${rootState.settings.location}&format=json`, {})
+            fetch(`${api.proxyurl + api.baseUrl}location.name?accessId=${api.key}&input=${rootState.settings.location}&format=json`, {})
                 .then(response => response.json())
                 .then(data => {
                     data.stopLocationOrCoordLocation ? commit("SET_LOCATION_DATA", data.stopLocationOrCoordLocation) : console.log("no location data");
@@ -24,6 +32,21 @@ export default {
                 .catch(function(error) {
                     console.error('Request failed', error)
                 });
+        },
+
+        fetchStationBoardData({ commit }, { stationId, type = 'departure' }) {
+            if(type === 'departure' || type === 'arrival') {
+                fetch(`${api.proxyurl + api.baseUrl + type}Board?format=json&accessId=${api.key}&id=${stationId}`, {})
+                .then(response => response.json())
+                .then(data => {
+                    commit("SET_CURRENT_STATION_BOARD", data[Capitalize(type)]);
+                })
+                .catch(function(error) {
+                    console.error('Request failed', error);
+                });
+            } else {
+                console.error("Accepted types are 'arrival' order 'departure'");
+            }
         }
     }
 };
--- src/utils/rmv-api.js
+++ src/utils/rmv-api.js
@@ -1,4 +1,6 @@
 export default ({
     baseUrl: '//www.rmv.de/hapi/',
     key: '<RMV-API-KEY>',
+    baseUrl: 'https://www.rmv.de/hapi/',
+    proxyurl: 'https://cors-anywhere.herokuapp.com/',
 });
