fbeb3220339430dae6def1da2bbe2617ec4a0f03
samsour <sam.sauer@hotmail.de>
2020-02-23 01:41:12 +0100

    Split rmv api into stops and coordinates

--- src/store/modules/rmv.js
+++ src/store/modules/rmv.js
@@ -4,21 +4,30 @@ import { Capitalize } from '@/utils/helpers';
 export default {
     namespaced: true,
     state: {
-        locationData: [],
-        currentStationBoardData: []
+        locations: {},
+        stationBoardData: []
     },
     getters: {
         // get actual stops, not the meta objects
-        currentStops: state => state.locationData.length > 0 ? state.locationData.filter(stop => stop.meta !== true) : [],
-        currentStationBoardData: state => state.currentStationBoardData
+        allCurrentLocations: state => {
+            if (state.locations.stops && state.locations.coords) {
+                return state.locations.stops.concat(state.locations.coords);
+            } else {
+                return state.locations.stops || state.locations.coords;
+            }
+        },
+        currentStops: state => state.locations.stops,
+        currentCoords: state => state.locations.coords,
+        stationBoardData: state => state.stationBoardData
     },
     mutations: {
-        SET_LOCATION_DATA(state, data) {
-            state.locationData = data.map(x => x.StopLocation)
+        SET_PUBLIC_TRANSPORT_LOCATIONS(state, data) {
+            state.locations.stops = data.filter(x => x.StopLocation);
+            state.locations.coords = data.filter(x => x.CoordLocation);
         },
 
         SET_CURRENT_STATION_BOARD(state, data) {
-            state.currentStationBoardData = data;
+            state.stationBoardData = data;
         },
     },
     actions: {
@@ -26,7 +35,7 @@ export default {
             fetch(`${api.proxyurl + api.baseUrl}location.name?accessId=${api.key}&input=${rootState.settings.location.city}&format=json`, {})
                 .then(response => response.json())
                 .then(data => {
-                    data.stopLocationOrCoordLocation ? commit("SET_LOCATION_DATA", data.stopLocationOrCoordLocation) : console.log("no location data");
+                    data.stopLocationOrCoordLocation ? commit("SET_PUBLIC_TRANSPORT_LOCATIONS", data.stopLocationOrCoordLocation) : console.log("no location data");
                 })
                 .catch(function(error) {
                     console.error('Request failed', error)
